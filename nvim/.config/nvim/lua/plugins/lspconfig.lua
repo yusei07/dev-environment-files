1a879038f3ac1da46b2b4be626c50acaf8dde6fe
anak0020 <anak0020@student.monash.edu>
2026-08-16 12:59:16 +1000

    manual lsp setup

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,18 +1,18 @@
 return {
   'neovim/nvim-lspconfig',
   config = function()
-    local lspconfig = require 'lspconfig'
+    local lspconfig = require('lspconfig')
 
     -- graphql
-    lspconfig.graphql.setup {
+    lspconfig.graphql.setup({
       filetypes = {
         'graphql',
         'gql',
       },
-    }
+    })
 
     -- lua
-    lspconfig.lua_ls.setup {
+    lspconfig.lua_ls.setup({
       settings = {
         Lua = {
           diagnostics = {
@@ -20,6 +20,16 @@ return {
           },
         },
       },
-    }
+    })
+
+    lspconfig.vimls.setup({}) -- vim-language-server
+    lspconfig.html.setup({})
+    lspconfig.tailwindcss.setup({})
+    lspconfig.ts_ls.setup({})
+    lspconfig.cssls.setup({})
+    lspconfig.pylsp.setup({})
+    lspconfig.emmet_ls.setup({
+      filetypes = { 'html', 'css', 'javascriptreact', 'typescriptreact' },
+    })
   end,
 }
