d3721577cc09ba84b8195b5913f29f1bb0f835ca
anak0020 <anak0020@student.monash.edu>
2026-08-17 11:20:59 +1000

    fix esc sytax err

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,17 +3,17 @@ return {
   tag = '0.1.4',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local builtin = require 'telescope.builtin'
+    local builtin = require('telescope.builtin')
     local keymap = vim.keymap
     keymap.set('n', '<leader>f', builtin.find_files, {})
     keymap.set('n', '<leader>g', builtin.live_grep, {})
 
-    local actions = require 'telescope.actions'
-    require('telescope').setup {
+    local actions = require('telescope.actions')
+    require('telescope').setup({
       defaults = {
         mappings = {
           i = {
-            ['esc'] = actions.close,
+            ['<esc>'] = actions.close,
           },
         },
         file_ignore_patterns = {
@@ -38,6 +38,6 @@ return {
           preview_width = 0.5,
         },
       },
-    }
+    })
   end,
 }
